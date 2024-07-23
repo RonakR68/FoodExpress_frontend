584e2c1ebe62ec0887912c9d8a96e7d0798143a5
Ronak <redkarronak@gmail.com>
2024-07-23 12:04:21 +0530

    cors

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://apifoodexpress.vercel.app'
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
